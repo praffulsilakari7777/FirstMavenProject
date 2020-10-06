4a3afe9bc6277d22303e3840df814da7ea298620
Prafful Silakari <praffulsilakari7777@gmail.com>
2020-10-06 12:46:57 +0530

    second commit

--- src/main/java/First_class.java
+++ src/main/java/First_class.java
@@ -1,7 +1,7 @@
 
 public class First_class {
     public static void main(String[] args) {
-        System.out.println("I Work in Amdocs");
+        System.out.println("I Work in Amdocs.");
     }
 
 }
